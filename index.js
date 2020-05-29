b260c2e3935e081fc70d71d0bb1fa6f2ecefa755
parikshit singh <pk223933@gmail.com>
2020-05-29 15:18:48 +0530

    BG dynamic, body pos,

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-const express = require('express');
+/* const express = require('express');
 const path = require('path');
 const port = 8000;
-
 const app = express();
-
 app.set( 'view engine', 'ejs' );
 app.set( 'views', path.join(__dirname, 'views') );
-app.use(express.urlencoded());
-
-
-
-
-
-
+app.use(express.urlencoded({extended:true}));
+// custom middleware 1
+app.use(function(req, res, next)
+{
+    req.myname="parikshit singh sin sin war"
+    // console.log("middleware1 called");
+    next();
+})
+// custom middleware 2
+app.use((req, res, next)=>
+{
+    console.log("my name from middileware 2", req.myname);
+    // console.log("middleware2 called");
+    next();
+})
 var contactList=[
     {
         name:"parikshit",
@@ -27,14 +33,9 @@ var contactList=[
         phone:'3948712481342'
     }
 ]
-
-
-
-
-
-
 app.get('/', (request, response) =>
 {
+    console.log(request.myname);
     return response.render('home', {
         title:"rendered",
         contact_list:contactList,
@@ -44,20 +45,11 @@ app.get('/added_contact', (req, res)=>
 {
     return res.render('added_contact', {});
 });
-
 app.post('/create-contact', (request, response)=>
 {
-    contactList.push({name:request.body.name, phone:request.body.phone});
+    contactList.push(request.body);
     return response.redirect('back');
 });
-
-
-
-
-
-
-
-
 app.listen(port, (error) =>
 {
     if (error)
@@ -68,5 +60,49 @@ app.listen(port, (error) =>
     else
     {
         console.log('server is running on the port', port);
+    }
+}); */
+
+
+
+
+
+const express=require('express');
+const port=8000;
+const path=require('path');
+
+const app=express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({extended:true}));
+app.use(express.static('assets'));
+
+var contactList=[];
+
+app.get('/', (req, res)=>
+{
+    var options={
+        title:"Contact List",
+        contact_list:contactList
+    };
+    return res.render('home', options);
+});
+app.post('/create-contact', (req, res)=>
+{
+    contactList.push(req.body);
+    return res.redirect('back');
+});
+
+app.listen(port, (error)=>
+{
+    if(error)
+    {
+        console.log("there was some error in starting the server");
+        return;
+    }
+    else
+    {
+        console.log("server is running on the port", port);
     }
 });
